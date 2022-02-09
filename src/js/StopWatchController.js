92adb98d6f3a8b9f53fa5ed1e1a33ddecf37341f
maycon.huanca <maycon.huanca@CLBBRC02RP8BJG8WP.local>
2022-02-09 15:39:32 -0300

    fixing bugs and refactoring

---
+++ src/js/StopWatchController.js
@@ -0,0 +1,5 @@
+class StopWatchController {
+    constructor() {
+
+    }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,15 @@
 const listOfLapsEl = document.getElementById("list-of-laps");
 const laps = [];
 
-const btnRestartEl = document.getElementById("btn-restart");
-const btnLapEl = document.getElementById("btn-lap");
-const timer = document.getElementById("timer");
 const btnStartEl = document.getElementById("btn-start");
+const btnRestartTimerEl = document.getElementById("btn-restart-timer");
+const btnResetLapsEl = document.getElementById("btn-reset-laps");
+const btnLapEl = document.getElementById("btn-lap");
+
+const timerEl = document.getElementById("timer");
 
 btnStartEl.addEventListener('click', event => {
     btnStartEl.classList.toggle('pressed-btn');
-
-    showBtnRestart();
 
     if (btnStartEl.textContent.trim() === "START") {
         btnStartEl.textContent = "STOP"
@@ -18,27 +18,33 @@ btnStartEl.addEventListener('click', event => {
         btnStartEl.textContent = "START"
         stopCounter();
     }
+
+    showBtnRestart();
     playAudioButton();
 });
 
 btnLapEl.addEventListener('click', event => {
     btnLapEl.classList.toggle("pressed-btn")
-    playAudioButton();
     setTimeout(() => btnLapEl.classList.toggle("pressed-btn"), 100)
 
+    playAudioButton();
     recordLap();
-
 });
 
-btnRestartEl.addEventListener('click', event => {
+btnResetLapsEl.addEventListener('click', event => {
+    laps.length = 0;
+    showLaps();
+});
+
+btnRestartTimerEl.addEventListener('click', event => {
     this.initialTimestamp = 0;
     displayTimestamp();
-    btnRestartEl.style.display = "none";
+    btnRestartTimerEl.style.display = "none";
 })
 
 
 function startCounter() {
-    if (!this.initialTimestamp) {
+    if (!timerHasStarted()) {
         this.initialTimestamp = 0
     }
 
@@ -55,18 +61,13 @@ function updateCount() {
 }
 
 function displayTimestamp() {
-    let date = new Date(this.initialTimestamp);
-    let minutes = date.getMinutes().toString();
-    let seconds = date.getSeconds().toString();
-    let miliseconds = date.getMilliseconds().toString();
-    this.timer.textContent = `${minutes.padStart(2, '0')}:${seconds.padStart(2, '0')}:${miliseconds.padStart(3, '0')}`;
+    let { minutes, seconds, miliseconds } = getTimestampValues();
+    timerEl.textContent = getFormatTimestamp({ minutes, seconds, miliseconds });
 }
 
 function showBtnRestart() {
-    this.btnRestartEl = document.getElementById("btn-restart");
-    if (this.initialTimestamp) {
-        this.btnRestartEl.style.display = "block"
-    }
+    if (timerHasStarted())
+        btnRestartTimerEl.style.display = "block"
 }
 
 function playAudioButton() {
@@ -74,28 +75,50 @@ function playAudioButton() {
 }
 
 function recordLap() {
-    if (this.initialTimestamp != 0 && this.initialTimestamp) {
-        let date = new Date(this.initialTimestamp);
-        let stringDate = `${date.getMinutes()}:${date.getSeconds()}:${date.getMilliseconds()}`;
-        laps.push(stringDate);
+    if (timerHasStarted()) {
+        laps.push(getFormatTimestamp(getTimestampValues()));
         showLaps();
     }
-
 }
 
 function showLaps() {
     listOfLapsEl.innerHTML = "";
-    laps.forEach((lap, index) => {
+    let lapsCopy = lapsCopy.slice();
 
+    lapsCopy.reverse().forEach((lap, index) => {
         let [minutes, seconds, miliseconds] = lap.split(":");
-
-        let divLap = `                        
-        <div class="container-lap">
-            <p>Lap ${index + 1} - ${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${miliseconds.padStart(3, '0')}</p>
-            <br/>
-            <hr/>
-        </div>`
-
+        let divLap = createLap({ minutes, seconds, miliseconds, index, length: laps.length });
         listOfLapsEl.innerHTML += divLap;
     })
+}
+
+function createLap({ minutes, seconds, miliseconds, index, length }) {
+    let div =
+        `<div class="container-lap">
+        <p class="detach">Lap ${(length - index).toString().padStart(2, '0')} - 
+        ${getFormatTimestamp({
+            minutes, seconds, miliseconds
+        })}
+    </p>
+        <br/>
+        <hr/>
+    </div>`
+
+    return div;
+}
+
+function timerHasStarted() {
+    return this.initialTimestamp != 0 && this.initialTimestamp;
+}
+
+function getFormatTimestamp({ minutes, seconds, miliseconds }) {
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${miliseconds.toString().padStart(3, '0')}`;
+}
+
+function getTimestampValues() {
+    let date = new Date(this.initialTimestamp);
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let miliseconds = date.getMilliseconds();
+    return { minutes, seconds, miliseconds };
 }
